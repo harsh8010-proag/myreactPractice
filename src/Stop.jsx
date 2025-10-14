e7391d06433e80986d9d0ab0f0578950510545c5
Harish Wavare <harishwavare@gmail.com>
2025-10-14 09:06:19 +0530

    i completed react sucssesfully

--- src/Stop.jsx
+++ src/Stop.jsx
@@ -45,7 +45,7 @@ useEffect(() =>{
   houers=String(houers).padStart(2,'0');
   minutes=String(minutes).padStart(2,'0');
   seconds=String(seconds).padStart(2,"0");
-  seconds=String(millisecond).padStart(2,"0");
+   millisecond=String(millisecond).padStart(2,"0");
   
 
 
